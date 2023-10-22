7d4ebaf4939af4a27250497f0921f902c2337000
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-10-22 12:57:41 +0500

    improvements and redesign

--- javascript/login.js
+++ javascript/login.js
@@ -29,3 +29,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const modeToggle = document.getElementById("mode-toggle");
     modeToggle.addEventListener("click", toggleMode);
 });
+
+
+// Login validating
+
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const errorHandler = document.getElementById('error')
+
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+
+    if (username.value === '' || username.value === null) {
+        messages.push('Username is required!')
+    }
+
+    if (password.value.length <= 6) {
+        messages.push('Password must be more than 6 characters!')
+    }
+
+    if (password.value.length >= 20) {
+        messages.push('Password must be less than 20 characters!')
+    }
+
+    if (password.value === 'password') {
+        messages.push('Password cannot be password')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorHandler.innerText = messages.join(', ')
+    }
+})
--- javascript/register.js
+++ javascript/register.js
@@ -29,3 +29,47 @@ document.addEventListener("DOMContentLoaded", function () {
     const modeToggle = document.getElementById("mode-toggle");
     modeToggle.addEventListener("click", toggleMode);
 });
+
+
+// Registration validating 
+
+const form = document.getElementById('form')
+const firstName = document.getElementById('firstname')
+const lastName = document.getElementById('lastname')
+const emailUsername = document.getElementById('email_username')
+const password = document.getElementById('password')
+const errorHandler = document.getElementById('error')
+
+
+form.addEventListener('submit', (e) => {
+    let messages = []
+
+    if (firstName.value === '' || firstName.value === null) {
+        messages.push('First name is required!')
+    }
+
+    if (lastName.value === '' || firstName.value === null) {
+        messages.push('Last name is required!')
+    }
+
+    if (emailUsername.value === '' || emailUsername.value === null) {
+        messages.push('Email or Username is required!')
+    }
+
+    if (password.value.length <= 6) {
+        messages.push('Password must be more than 6 characters!')
+    }
+
+    if (password.value.length >= 20) {
+        messages.push('Password must be less than 20 characters!')
+    }
+
+    if (password.value === 'password') {
+        messages.push('Password cannot be password')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorHandler.innerText = messages.join(', ')
+    }
+})
