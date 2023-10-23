3a56e2c1e64cf9e8d7dfa681c21b4b0bf79fb73c
Mirjalol Ziyadullayev <121923945+mirjalolziyadullayev@users.noreply.github.com>
2023-10-23 15:16:28 +0500

    Improvements and adding user registration feature

--- javascript/register.js
+++ javascript/register.js
@@ -33,43 +33,66 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Registration validating 
 
-const form = document.getElementById('form')
-const firstName = document.getElementById('firstname')
-const lastName = document.getElementById('lastname')
-const emailUsername = document.getElementById('email_username')
-const password = document.getElementById('password')
-const errorHandler = document.getElementById('error')
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const emailUsername = document.getElementById('email_username');
+const password = document.getElementById('password');
+const errorHandler = document.getElementById('error');
 
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-form.addEventListener('submit', (e) => {
-    let messages = []
+    let messages = [];
 
     if (firstName.value === '' || firstName.value === null) {
-        messages.push('First name is required!')
+        messages.push('First name is required!');
     }
 
-    if (lastName.value === '' || firstName.value === null) {
-        messages.push('Last name is required!')
+    if (lastName.value === '' || lastName.value === null) {
+        messages.push('Last name is required!');
     }
 
     if (emailUsername.value === '' || emailUsername.value === null) {
-        messages.push('Email or Username is required!')
+        messages.push('Email or Username is required!');
     }
 
-    if (password.value.length <= 6) {
-        messages.push('Password must be more than 6 characters!')
+    if (password.value.length < 6) {
+        messages.push('Password must be at least 6 characters!');
     }
 
-    if (password.value.length >= 20) {
-        messages.push('Password must be less than 20 characters!')
+    if (password.value.length > 20) {
+        messages.push('Password must be less than 20 characters!');
     }
 
     if (password.value === 'password') {
-        messages.push('Password cannot be password')
+        messages.push('Password cannot be "password"');
     }
 
     if (messages.length > 0) {
-        e.preventDefault()
-        errorHandler.innerText = messages.join(', ')
+        errorHandler.innerText = messages.join(', ');
+        return;
     }
-})
+
+    // Creating a new user object
+    const newUser = {
+        FirstName: firstName.value,
+        LastName: lastName.value,
+        EmailUsername: emailUsername.value,
+        Password: password.value
+    };
+
+    // Send user data as JSON to the server
+
+    fetch("http://192.168.1.246:8080/users", {
+    method: "POST",
+    body: JSON.stringify(newUser),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+    },
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    form.reset();
+    errorHandler.innerText = "Data saved successfully";
+});
